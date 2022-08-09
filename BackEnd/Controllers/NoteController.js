ac581de6028d2eab41bed270ce3928cbe4f3b27f
Heshan <mheshan27@gmail.com>
2022-08-09 23:28:54 +0530

    Update NoteController.js

--- BackEnd/Controllers/NoteController.js
+++ BackEnd/Controllers/NoteController.js
@@ -3,38 +3,45 @@ const { CustomAPIError } = require("../Errors/errorClases");
 const noteModel = require("../Models/NoteModel");
 
 const getNotesDetails = async (req, res) => {
+  //get query values
   const search = String(req.query.search);
   const id = String(req.query.id);
-  console.log(id);
+
+  //get query values and set default values if not provided
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 5;
+  //calculate skip value
   const skip = (page - 1) * limit;
+
+  //get count of all notes
   let notesCount = await noteModel.find({ createdBy: id }).count();
+  //get all notes and search by title and sort according to user who createdBy
+  //and limit the result according to page and limit
   let notes = await noteModel
     .aggregate([
       {
         $match: {
           createdBy: mongoose.Types.ObjectId(id),
-          $or: [
-            { title: { $regex: search, $options: "si" } },
-            { discription: { $regex: search, $options: "i" } },
-          ],
+          $or: [{ title: { $regex: search, $options: "si" } }],
         },
       },
     ])
     .sort({ createdAt: -1 })
     .skip(skip)
     .limit(limit);
-  console.log(notes);
+
+  //calculate total pages
   notesCount = Math.ceil(notesCount / limit);
   res.status(200).json({ notes, notesCount });
 };
 
+//create new note
 const postNoteDetails = async (req, res) => {
   const note = await noteModel.create(req.body);
   res.status(200).json({ note });
 };
 
+//update  note
 const updateNoteDetails = async (req, res) => {
   console.log(req.body);
   const note = await noteModel.findByIdAndUpdate(req.params.id, req.body, {
@@ -44,6 +51,7 @@ const updateNoteDetails = async (req, res) => {
   res.status(200).json(note);
 };
 
+//delete exsisting note
 const deleteNoteDetails = async (req, res) => {
   try {
     const note = await noteModel.findByIdAndDelete(req.params.id, {
@@ -55,6 +63,7 @@ const deleteNoteDetails = async (req, res) => {
   }
 };
 
+//export all functions
 module.exports = {
   getNotesDetails,
   postNoteDetails,
